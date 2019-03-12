04edc457063d45447e04e65b0ff765f3325d5f08
Sedat Guzelsemme <sedatguzelsemme@gmail.com>
2019-03-12 22:50:27 +0300

    initial commit

---
+++ target/classes/com/dasrc/RSMSAPI/RsmsapiApplication.kt
@@ -0,0 +1,11 @@
+package com.dasrc.RSMSAPI
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RsmsapiApplication
+
+fun main(args: Array<String>) {
+	runApplication<RsmsapiApplication>(*args)
+}
